d40db076cea922c7d840da2b8ea5f3c6b9cddead
Julia Perera <julia-perera@web.de>
2023-09-02 13:24:55 +0200

    Added navbar into Hbs layout, Added hbs views + Week model

---
+++ models/Week.js
@@ -0,0 +1,24 @@
+
+const { Schema, model } = require("mongoose");
+
+// Create data for Week model
+/*
+--> Which data does the Week model need?
+
+const weekSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: false,
+      unique: true,
+      trim: true,
+    },
+  }
+);
+
+
+const Week = model("Week", weekSchema);
+
+module.exports = Week;
+
+*/
