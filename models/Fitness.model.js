113ed84724eca3842ae65948882fcb11e97a0442
Alejandro Ahijado Ibarra <alejandro.ahijado@gmail.com>
2023-09-03 19:20:22 +0200

    Create path to fitness route, finish fitness mdl

--- models/Fitness.model.js
+++ models/Fitness.model.js
@@ -4,28 +4,36 @@ const fitnessSchema = new Schema(
   {
     name: {
       type: String,
-      required: true,
     },
     force: {
       type: String,
-      required: false,
       enum: ["pull", "push", "static"],
     },
     level: {
         type: String,
-        required: false,
         enum: ["beginner", "intermediate", "expert"]
     },
-    muscle: {
-      type: String,
-      required: false,
-      enum: ["abdominals", "abductors", "adductors", "biceps", "calves", "chest", "forearms", "glutes", "hamstrings", "lats", "lower_back", "middle_back", "neck", "quadriceps", "traps", "triceps"]
+    primaryMuscles: {
+      type: [String],
+      enum: ["abdominals", "abductors", "adductors", "biceps", "calves", "chest", "forearms", "glutes", "hamstrings", "lats", "middle back", "neck", "shoulders", "quadriceps", "traps", "triceps"]
     },
-    
-    offset: {
-        type: Number,
-        default: 0
-    }
+    secondaryMuscles: {
+        type: [String],
+        enum: ["abdominals", "abductors", "adductors", "biceps", "calves", "chest", "forearms", "glutes", "hamstrings", "lats","lower back", "middle back", "neck", "shoulders", "quadriceps", "traps", "triceps"]
+      },
+    mechanic: {
+        type: String,
+        enum: ["compound", "isolation"]
+    },
+    equipment: {
+        type: String,
+        enum: ["barbell", "body only", "other", "dumbbell", "kettlebells", "cable", "machine", "medicine ball", "bands", "e-z curl bar"]
+    },
+    category: {
+        type: String,
+        enum: ["strength", "stretching", "plyometrics", "powerlifting", "olympic weightlifting", "strongman", "cardio"]
+    },
+    _id: '64f20ca5ebc46a5385fd9c97'
   },
   {
     timestamps: true,
