a38011943c5b8a19c658d0570889ffa1c68272b2
Alejandro Ahijado Ibarra <alejandro.ahijado@gmail.com>
2023-09-04 16:50:28 +0200

    Changes in hbs files and fitness and week models

--- routes/week.route.js
+++ routes/week.route.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const mongoose = require("mongoose");
 
 // Not sure we need this
-/* const exerciseData = require /* Where is Json file? */; // import Json file */
 
 
 /* Not really a route, let's see where to put it */
